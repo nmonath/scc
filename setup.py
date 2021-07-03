6f693814babd716d05f464b1b3f58d281c90856f
Nicholas Monath <nmonath@cs.umass.edu>
2021-07-03 12:46:27 -0400

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='scc',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/nmonath/scc',
       author='Nicholas Monath',
       author_email='nmonath@cs.umass.edu',
